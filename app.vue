16be358735be5642b2d2c5021879d0ba64d40749
Sergey Yakovenko <51407490+yakovenko-sergey@users.noreply.github.com>
2021-02-11 16:04:24 +0200

    Add reactiveGetter and reaciveSetter for textsArr

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div>
+          <p>Menu</p>
+          <ul>
+              <li>TR1</li>
+              <li>TR2</li>
+              <li>TR3</li>
+              <li>TR4</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['text','param']
+    }
+</script>
---
+++ param-template.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="parametr-container">
+        <div class="text_parametr"><p>TEXT HERE</p></div>
+        <div class="value-parametr-block">
+            <div><p id="Temp-Press-Style">PARAM HERE</p></div>
+            <div class="temp_ed">°C</div>
+        </div>
+        <div class="border_div"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['text','param']
+    }
+</script>
